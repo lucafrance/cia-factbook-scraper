8184a56e3cea4eb2c4d66acbafa03571ec7171e9
Luca Franceschini <luca.france@outlook.com>
2022-07-27 16:17:32 +0200

    Add parsing of strong tags as separate columns

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 `country_data_codes.csv` downloaded from [here](https://www.cia.gov/the-world-factbook/references/country-data-codes/).
 
 ### Possible improvements
-- Scrape the countries' photos.
-- Scrape the CIA world factbook from previous years.
-- Split lists in multiple columns, e.g. "Area" is currently just one column:
-  - `total: 301,340 sq km`
-  - `land: 294,140 sq km`
-  - `water: 7,200 sq km`
-  - `note: includes Sardinia and Sicily`
-- Remove text from numerical values and add text to column name.
-  - Now: `column name = "Coastline"`, `value = "7,600 km"`
-  - After: `column name = "Coastline (sq km)"`, `value = 7600`
+Export list of columns.
+
+Scrape the countries' photos.
+
+Scrape audio samples.
+
+Scrape the CIA world factbook from previous years.
+
+Remove text from numerical values and add text to column name.
+- Now: `column name = "Coastline"`, `value = "7,600 km"`
+- After: `column name = "Coastline (sq km)"`, `value = 7600`
+
+Refractor code that handles `strong` tags.
+
+Rename `next_tag` variable to something more meaningful.
--- cia_factbook_scaper.py
+++ cia_factbook_scaper.py
@@ -1,10 +1,11 @@
+from cgitb import text
 import json
 import logging
 import os
 import pandas as pd
 import pickle
 
-from bs4 import BeautifulSoup
+import bs4
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -55,7 +56,7 @@ def parse_pages(countries):
 
     for country in countries.values():
         page_source = country.pop("page_source")
-        soup = BeautifulSoup(page_source, "html.parser")
+        soup = bs4.BeautifulSoup(page_source, "html.parser")
         content = soup.find("div", class_="content-area-content")
         for section in content.children:
             h2_tag = section.find("h2")
@@ -65,7 +66,66 @@ def parse_pages(countries):
             for h3 in section.find_all("h3"):
                 column_name = ": ".join([section_name, h3.text])
                 next_tag = h3.next_sibling
-                if next_tag.text != "":
+
+                # If `next_tag` is a bs4 Tag, then it could contain `strong`` tags.
+                # If not, it is ptobably a bs4 NavigableString and the list of `strong` tags is empty.
+                if type(next_tag) is bs4.element.Tag:
+                    strong_tags = next_tag.find_all("strong")
+                else:
+                    strong_tags = []
+                
+                # If there are `strong` tags, each of them gets a separate column.
+                # This case probably deserves its own function.
+                if len(strong_tags) > 0:
+                    # There might be information before the first `strong` tag.
+                    # If so, it gets its own column.
+                    if not next_tag.text.startswith(strong_tags[0].text):
+                        country[column_name] = next_tag.text.split(strong_tags[0].text, 1)[0]
+                    
+                    # Each `strong` tag gets its own column
+                    last_strong_tag = None
+                    for i in range(len(strong_tags) - 1):
+                        
+                        if strong_tags[i].text == "":
+                            continue
+                        column_name2 = " - ".join([column_name, strong_tags[i].text.rstrip(":")])
+                        txt = next_tag.text
+                        txt = txt.split(strong_tags[i].text, 1)[1]
+                        # This is to handle a stupid case of `<strong><br></strong>` in the source.
+                        # If no proper `strong` tag is left, then that is the last tag of the section
+                        # and is handeled separetely.
+                        # I feel like there is a smarter way to do this.
+                        next_non_empty_strong_tag = None
+                        for j in range(i+1, len(strong_tags)):
+                            if strong_tags[j].text.strip() != "":
+                                next_non_empty_strong_tag = strong_tags[j]
+                                break
+                        if next_non_empty_strong_tag is None:
+                            last_strong_tag = strong_tags[i]
+                            break
+                        else:
+                            txt = txt.split(strong_tags[j].text, 1)[0]
+                        country[column_name2] = txt.strip()
+                    
+                    if last_strong_tag is None:
+                        last_strong_tag = strong_tags[-1]
+                    if last_strong_tag.text.strip() != "":
+                        column_name2 = " - ".join([column_name, last_strong_tag.text.rstrip(":")])
+                        txt = next_tag.text
+                        txt = txt.split(last_strong_tag.text, 1)[1]
+                        # Find the tag starting the next subsection, which will start with a `h2` tag.
+                        # If none is found, just keep whatever is left.
+                        next_subsection_tag = None
+                        for tag in last_strong_tag.next_siblings:
+                            if repr(tag).startswith("<h2"):
+                                next_subsection_tag = tag
+                                break
+                        if next_subsection_tag is not None:
+                            txt.split(next_subsection_tag.text)[0]
+                        country[column_name2] = txt.strip()
+                    
+                # If there are no `strong` tags, then the whole section's text is taken
+                elif next_tag.text != "":
                     country[column_name] = next_tag.text
                 elif next_tag.next_sibling is None:
                     logging.warning("I could not find a paragraph for section \"{}\" on \"{}\".".format(section_name, country["url"]))
